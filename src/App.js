baca95ce21bc9a8ad074d7b20d18b3367916da17
livermore2025 <livermore2025@gmail.com>
2022-03-23 17:09:06 +0900

    cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigator from "./components/Navigator";
 import ControlledTabs from "./components/ControlledTabs";
+import Cards from "./components/Cards";
 
 const App = () => {
   return (
     <div>
       <Navigator />
       <ControlledTabs />
+      <Cards />
     </div>
   );
 };
