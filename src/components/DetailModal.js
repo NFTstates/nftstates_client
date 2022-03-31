3d4b6dabb50bcd13a81dedf48a8ba0e7b4058fba
livermore2025 <livermore2025@gmail.com>
2022-03-31 09:57:59 +0900

    입력UI_50%

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -29,19 +29,55 @@ function DetailModal(props) {
         <Figure>
           <Image />
           <div>
-            <h4>원하는 대출 기간</h4>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-            <button>제출</button>
+            <h4>원하는 대출 조건 : 6000UST 90일 </h4>
+            <form>
+              <span>
+                <label>
+                  <span>대출 금액</span>
+                  <span>
+                    <input
+                      name="loanValue"
+                      type="number"
+                      min="1"
+                      required
+                      value
+                    />
+                    <select
+                      class="loan-currency"
+                      name="selectCurrency"
+                      required
+                    >
+                      <option>Klay</option>
+                      <option>stableCoin</option>
+                    </select>
+                  </span>
+                </label>
+              </span>
+              <br />
+              <span>
+                <label>
+                  <span>기간</span>
+                  <select class="duration" name="duration" required>
+                    <option value="7">7일</option>
+                    <option value="14">14일</option>
+                    <option value="30">30일</option>
+                    <option value="90">90일</option>
+                  </select>
+                </label>
+              </span>
+              <span>
+                <label></label>
+              </span>
+              <span>
+                <label></label>
+              </span>
+            </form>
+            <button>제안하기</button>
           </div>
         </Figure>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={props.onHide}>닫기</Button>
       </Modal.Footer>
     </Modal>
   );
